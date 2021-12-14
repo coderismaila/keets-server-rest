830f284177c92d72d4c9bd1db0d2854a733dcadd
Ismaila Hassan <code.ismailah28@gmail.com>
2021-12-14 17:08:01 +0100

    implement signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('login')
+  @Post('signin')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<any> {
+    return this.authService.signup(createUserDto);
   }
 
   @UseGuards(JwtAuthGuard)
