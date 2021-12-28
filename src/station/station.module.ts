0b4f3635ae1274027bb062398c969de6604f04bb
Ismaila Hassan <code.ismailah28@gmail.com>
2021-12-28 02:51:21 +0100

    fix error in file name

--- src/station/station.module.ts
+++ src/station/station.module.ts
@@ -3,7 +3,7 @@ import { StationService } from './services/station.service';
 import { StationController } from './controllers/station.controller';
 import { PrismaService } from 'src/prisma.service';
 import { PowerTransformerService } from './services/power-transformer.service';
-import { PowerTransformerController } from './controllers/power-transformer.controller.dto';
+import { PowerTransformerController } from './controllers/power-transformer.controller';
 
 @Module({
   controllers: [StationController, PowerTransformerController],
